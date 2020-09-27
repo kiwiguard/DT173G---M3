5ada9b7366cf0b2446d3b2d1fe658287e466f9e7
Kiwiguard <frida.susanne@gmail.com>
2020-09-27 18:11:12 +0200

    Added content to home-page and a mobile menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,1 +1,8 @@
 // Mobile-menu scripts
+function openNav() {
+    document.getElementById("mobile-nav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mobile-nav").style.width = "0";
+}
